076f71c5d00c3344cc0bd5b0e94852a01885b786
Jean-Marc McGhee <jeanmarc.mcghee@gmail.com>
2019-09-02 01:33:11 +0100

    Changed comments. Added Post route.

--- fs-practice/audio/streaming/practice-1/server.js
+++ fs-practice/audio/streaming/practice-1/server.js
@@ -1,4 +1,4 @@
-// npm packages
+// Npm packages
 const express      = require('express');
 const multer       = require('multer');
 
@@ -7,19 +7,19 @@ const {
   ObjectID, 
   GridFSBucket }   = require('mongodb');
 
-// node native
+// Node native
 const { Readable } = require('stream');
 
-// config
+// Config
 const config       = require('./config');
 
-// express intialization
+// Express initialization
 const routes       = express.Router();
 const app          = express();
 
 app.use('/audio', routes);
 
-// Mongo intialization
+// Mongo initialization
 let db;
 MongoClient.connect(config.mogodbHost, (err, database) => {
   if (err) {
@@ -28,7 +28,6 @@ MongoClient.connect(config.mogodbHost, (err, database) => {
   }
   db = database;
 });
-
 
 // GET route
 routes.get('/:trackId', (req, res) => {
@@ -52,6 +51,54 @@ routes.get('/:trackId', (req, res) => {
     }
 });
 
-// TODO: POST audio/ upload using multer and GridFSBucket
+app.post('/', (req, res) => {
+  const storage = multer.memoryStorage();
+  const upload = multer({
+    storage, 
+    limits: { 
+      fields: 1, 
+      fileSize: 6000000, 
+      files: 1, 
+      parts: 2 }
+  });
+
+  upload.single('track')(req, res, (err) => {
+    
+    // Error handling
+    if (err) {
+      return res.status(400).json({ message: 'Unable to upload the payload provided.'})
+    }  else if (!req.body.name) {
+      return res.status(400).json({ message: 'No track name in request body' });
+    }
+
+    const trackName           = req.body.name;
+
+    // Convert buffer into readable stream
+
+    const readableTrackStream = new Readable();
+    
+    readableTrackStream.push(req.file.buffer);
+    
+    // Ends Readable stream data
+    readableTrackStream.push(null);
+
+    const bucket = new GridFSBucket(db, {
+      bucketName: 'tracks'
+    });
+
+    const uploadStream = bucket.openUploadStream(trackName);
+
+    readableTrackStream.pipe(uploadStream);
+
+    uploadStream.on('error', () => {
+      return res.status(500).json({ message: "Error uploading file" });
+    });
+
+    uploadStream.on('finish', () => {
+      return res.status(201).json({ message: `File uploaded successfully, stored under Mongo ObjectID: ${uploadStream.id}`});
+    })
+    });
+
+});
 
 app.listen(3750, () => console.log('Express server is now running on http://localhost:3750'));
