399c045e7dfa89c2684153bab257ae4b2093462b
Jean-Marc McGhee <jeanmarc.mcghee@gmail.com>
2019-09-04 22:47:41 +0100

    Added JSDoc description and console.log to test.

--- algorithms/sorting/bubble/bubble.js
+++ algorithms/sorting/bubble/bubble.js
@@ -1,18 +1,23 @@
 const initialNumbers = [ 9, 12, 43, 55, 63, 45, 23, 4, 32, 5, 4, 54, 3, 5, 43, 64, 6436346, 436 ];
 
-// classic method - do while - bubble sort
-const bubbleSort1 = ( arr ) => { 
+/**
+ * @description A classic method of bubblesort using do while 
+ * @param {*} array 
+ */
+const bubbleSort = ( array ) => { 
     let swapped;
     do {
         swapped = false;
-        for(let i = 0; i < arr.length; i++) {
-            if (arr[i] > arr[i + 1]) {
-                let temp   = arr[i];
-                arr[i]     = arr[i + 1];
-                arr[i + 1] = temp;
+        for(let i = 0; i < array.length; i++) {
+            if (array[i] > array[i + 1]) {
+                let temp   = array[i];
+                array[i]     = array[i + 1];
+                array[i + 1] = temp;
                 swapped    = true;
             }
         }
     } while (swapped);
-    return arr;
+    return array;
 };
+
+console.log(bubbleSort(initialNumbers));
